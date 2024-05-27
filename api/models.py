e1b875dccfb96a3ce4848c919817aaad6906253e
Abdul Rahman <ar69king@gmail.com>
2024-05-27 19:05:45 +0500

    .

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+# class Settings(models.Model):
+#     settings_json = models.JSONField()
--- api/plugins/reminderHandle.py
+++ api/plugins/reminderHandle.py
@@ -13,7 +13,7 @@ def handle_function(message: Message):
         print(reminder["notes"])
         notes = json.loads(reminder["notes"])
         if notes["time_remaining"] == 0:
-            message.outgoing_text_message = f'*🔔 Reminder: Time\'s up for \"{reminder["title"]}\"*'
+            message.outgoing_text_message = f'*Time\'s up for \"{reminder["title"]}\"*'
         elif notes["time_remaining"] == 10:
             message.outgoing_text_message = f'*🔔 Reminder: Only {notes["time_remaining"]} minutes left for \"{reminder["title"]}\"*\n\nYou should start sbmitting your work now.\n\n{notes["link"]}'
         else:
