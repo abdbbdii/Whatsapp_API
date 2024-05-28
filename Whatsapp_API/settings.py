4267122eaec72dba1715b02135afc8387db852d2
Abdul Rahman <ar69king@gmail.com>
2024-05-28 11:45:38 +0500

    .

--- Whatsapp_API/settings.py
+++ Whatsapp_API/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     #     "ENGINE": "django.db.backends.sqlite3",
     #     "NAME": BASE_DIR / "db.sqlite3",
     # }
-    "default": dj_database_url.parse(os.getenv("DATABASE_URL")),
+    # "default": dj_database_url.parse(os.getenv("DATABASE_URL")),
 }
 
 
