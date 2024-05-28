967a082b03f69b22ab8a1563d7c14ff5bffdbe20
Abdul Rahman <ar69king@gmail.com>
2024-05-28 13:06:54 +0500

    .

--- Whatsapp_API/settings.py
+++ Whatsapp_API/settings.py
@@ -12,9 +12,9 @@ SECRET_KEY = os.getenv("DJANGO_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "vercel.app",
+    "*.vercel.app",
     "127.0.0.1",
-    "serveo.net",
+    "*.serveo.net",
     "localhost",
 ]
 
