9f7de0f19c1f2bcae4cf2ff9c17166287792e040
Abdul Rahman <ar69king@gmail.com>
2024-06-05 22:12:24 +0500

    .

--- api/plugins/settingsHandle.py
+++ api/plugins/settingsHandle.py
@@ -43,5 +43,5 @@ def parser(args: str):
 `/settings -g all`
 
 *Available settings:*
-{', '.join([attr for attr in dir(appSettings) if not callable(getattr(appSettings, attr)) and not attr.startswith('__')])}"""
+- {'\n - '.join([attr for attr in dir(appSettings) if not callable(getattr(appSettings, attr)) and not attr.startswith('__')])}"""
     return parse
