3771f6c7392250a23f0e24bacbf8aa0ca89118a4
Abdul Rahman <ar69king@gmail.com>
2024-06-12 23:06:53 +0500

    .

--- api/plugins/kharcheyHandle.py
+++ api/plugins/kharcheyHandle.py
@@ -12,14 +12,17 @@ pluginInfo = {
 
 def handle_function(message: Message):
 
-    def get_list():
+    def get_list() -> str:
         total = 0
         outgoing_text_message = ""
         for i, item in enumerate(Kharchey.objects.filter(group=message.group, sender=message.sender).order_by("date")):
             time = f"{item.date.day}/{item.date.month}/{item.date.year} {item.date.hour%12 if item.date.hour%12 != 0 else 12}:{item.date.minute} {'AM' if item.date.hour < 12 else 'PM'}"
             outgoing_text_message += f"{i+1}. `{time}` {item.item} {str(item.quantity)+'x' if item.quantity != 1 else ""}{item.price} = {item.quantity * item.price}\n"
             total += item.quantity * item.price
-        outgoing_text_message += f"\n*Total: {total}*"
+        if total:
+            outgoing_text_message += f"\n*Total: {total}*"
+        else:
+            outgoing_text_message = "List is empty"
         return outgoing_text_message
 
     if message.arguments[1] == "List" or message.arguments[1] == "list":
