3c0fcbe13ee76b89761f1c058b96b4ef641fffab
Abdul Rahman <ar69king@gmail.com>
2024-06-13 20:50:37 +0500

    .

--- api/whatsapp_api_handle.py
+++ api/whatsapp_api_handle.py
@@ -99,9 +99,7 @@ class Message:
 
     def process_incoming_text_message(self) -> None:
         if self.group:
-            print(self.incoming_text_message)
-            print(re.search(r'^\.(\s?\w+\.)+\w+', self.incoming_text_message))
-            if re.search(r'^\.(\s?\w+\.)+\w+', self.incoming_text_message):
+            if re.search(r'^\.(\s?\w+\.)+.+', self.incoming_text_message):
                 self.incoming_text_message = self.incoming_text_message.lstrip(".").strip()
             else:
                 raise MessageNotValid("Message does not start with a dot.")
