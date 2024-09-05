a4ebc20ad55ef04bc1116f551e35684f4780ed06
Abdul Rahman <ar69king@gmail.com>
2024-09-05 19:24:41 +0500

    patch

--- api/utils/download_gdrive.py
+++ api/utils/download_gdrive.py
@@ -34,12 +34,21 @@ def download_gdrive_file(gdrive_link):
     """Gets the file data in bytes from Google Drive given its link."""
     r = requests.get(
         appSettings.utils_server + "service/google_auth/",
-        json={"password": appSettings.utils_server_password, "scopes": ["https://www.googleapis.com/auth/drive.readonly"], "token_pickle_base64": appSettings.token_pickle_base64},
+        json={
+            "password": appSettings.utils_server_password,
+            "scopes": [
+                "https://www.googleapis.com/auth/drive.readonly",
+            ],
+            "token_pickle_base64": appSettings.token_pickle_base64,
+        },
     )
+
     if r.status_code != 200:
         return None
+
     if r.json().get("token_pickle_base64") != appSettings.token_pickle_base64:
-        appSettings.update('token_pickle_base64', r.json().get("token_pickle_base64"))
+        appSettings.update("token_pickle_base64", r.json().get("token_pickle_base64"))
+
     service = build("drive", "v3", credentials=pickle.loads(base64.b64decode(appSettings.token_pickle_base64)))
     file_id = get_file_id_from_link(gdrive_link)
     file_data = get_file_data(service, file_id)
